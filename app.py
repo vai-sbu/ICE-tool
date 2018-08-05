0311729d36cf0186b3a43f42125b5238ba87e7df
tyagi-iiitv <anjul.ten@gmail.com>
2018-08-04 21:49:55 -0400

    Corrected code for bars reacting to clicks

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 import json
 
-from flask import Flask, render_template, request, redirect, Response
+from flask import Flask, render_template, request, redirect, Response, jsonify
 import pandas as pd
 
 app = Flask(__name__)
-
-def before_request():
-    app.jinja_env.cache = {}
-
-app.before_request(before_request)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.jinja_env.auto_reload = True
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-
 
 def calculations(filtered_data):
     max_cols = 0
@@ -24,10 +15,12 @@ def calculations(filtered_data):
         df_grouped = pd.DataFrame(filtered_data[col_thp].groupby(col).describe()).reset_index()
         df_grouped['Max'] = df_grouped[('Throughput', 'max')]
         df_grouped['Min'] = df_grouped[('Throughput', 'min')]
-        chart_df = df_grouped[[col, 'Max', 'Min']]
+        new_chart_df = df_grouped[[col, 'Max', 'Min']]
+        new_chart_df.columns = new_chart_df.columns.droplevel(1)
+        chart_df = dataframes[col]
+        chart_df.loc[chart_df[col].isin(new_chart_df[col]), ['Max','Min']] = new_chart_df[['Max','Min']]
         max_thp = max(max_thp, chart_df['Max'].max())
         min_thp = min(min_thp, chart_df['Min'].min())
-        chart_df.columns = chart_df.columns.droplevel(1)
         max_cols = max(max_cols, chart_df.shape[0])
         chart_df = chart_df.to_dict(orient='records')
         chart_df = json.dumps(chart_df)
@@ -51,8 +44,12 @@ def index():
         column_received = request.form['column']
         value_received = request.form['value']
         switch_received = request.form['switch']
+        print(column_received)
+        print(value_received)
+        print(switch_received)
         filtered_data = data
         # Modifying the values based on selected bars
+        
         if switch_received == 'off':
             unique_values[column_received].remove(value_received)
         else:
@@ -61,18 +58,47 @@ def index():
         for item in columns[:-1]:
             filtered_data = filtered_data[filtered_data[item].isin(unique_values[item])]
         calculations(filtered_data)
+        return jsonify(data_tosend)
     else:
-        calculations(data)
-    return render_template("index.html", data=data_tosend)
+        for item in columns[:-1]:
+            unique_items = list(data[item].unique())
+            unique_values[item] = unique_items
+        
+        max_cols = 0
+        min_thp = 10000000000000000
+        max_thp = 0
+        for col in columns[:-1]:
+            col_thp = [col]
+            col_thp.append('Throughput')
+            df_grouped = pd.DataFrame(data[col_thp].groupby(col).describe()).reset_index()
+            df_grouped['Max'] = df_grouped[('Throughput', 'max')]
+            df_grouped['Min'] = df_grouped[('Throughput', 'min')]
+            chart_df = df_grouped[[col, 'Max', 'Min']]
+            dataframes[col] = chart_df
+            max_thp = max(max_thp, chart_df['Max'].max())
+            min_thp = min(min_thp, chart_df['Min'].min())
+            chart_df.columns = chart_df.columns.droplevel(1)
+            max_cols = max(max_cols, chart_df.shape[0])
+            chart_df = chart_df.to_dict(orient='records')
+            chart_df = json.dumps(chart_df)
+            data_tosend[col] = chart_df
+        data_tosend['Max Cols'] = max_cols
+        data_tosend['Max Thp'] = max_thp
+        data_tosend['Min Thp'] = min_thp
+        return render_template("index.html", data=data_tosend)
 
 
 if __name__ == "__main__":
     data = pd.read_csv('dataset/systems_data.csv')
     columns = list(data.columns)
     data_tosend = {'columns': columns[:-1]}
+    # Store the dataframes in a dictionary
+    dataframes = {}
     # Calculating unique values in each column
     unique_values = {}
     for item in columns[:-1]:
         unique_items = list(data[item].unique())
         unique_values[item] = unique_items
+    
+    
     app.run(debug=True)
--- static/js/index.js
+++ static/js/index.js
@@ -3,42 +3,19 @@ let result_svg_height = 500;
 let result_svg_width = 130;
 let result_bar_width = 120;
 let svgHeight = 300, barPadding = 25;
-
-
-function readBody(xhr) {
-    let text;
-    if (!xhr.responseType || xhr.responseType === "text") {
-        text = xhr.responseText;
-    } else if (xhr.responseType === "document") {
-        text = xhr.responseXML;
-    } else {
-        text = xhr.response;
-    }
-    return text;
-}
-
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-        console.log(readBody(xhr));
-        $("html").html(readBody(xhr));
-    }
-}
+let data_received;
+let selection = [];
 
 
 function redraw(){
 
     $('#area1').empty();   
     $('#area2').empty();
-
     for(let i in column){
         // Finding the max throughput for the given variable
         let dataset = JSON.parse(data_imported[column[i]]);
         let max_thp = dataset.map(function(o){return o.Max});
-        
         // Main code to create plots using the data
-        let selection = [];
-        
         let linearScale = d3.scaleLinear()
             .domain([0, d3.max(max_thp)])
             .range([0,svgHeight-25]);
@@ -61,25 +38,45 @@ function redraw(){
                 return linearScale(d.Max - d.Min); 
             })
             .attr("width", barWidth - barPadding)
+            .attr("opacity", function(d, j){
+                let is_present = false;
+                for(let k in selection){
+                    if(selection[k].id == column[i]+dataset[j][column[i]])
+                    is_present = true;
+                }
+                if(!is_present){
+                    return 1
+                }
+                else{
+                    console.log(column[i]+dataset[j][column[i]])
+                    return 0.5
+                }
+            })
             .attr("transform", function (d, i) {
                 let translate = [barWidth * i, 0]; 
                 return "translate("+ translate +")";
             })
             .on("click", function(d,j){
                 let data_toserver;
-                if(selection.indexOf(d) == -1){
-                    selection.push(d);
-                    d3.select(this).style("opacity", 0.5);
+                let is_present = false;
+                for(let k in selection){
+                    if(selection[k].id == column[i]+dataset[j][column[i]])
+                    is_present = true;
+                }
+                if(!is_present){
+                    selection.push({'id': column[i]+dataset[j][column[i]]});
+                    // d3.select(this).style("opacity", 0.5);
                     data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'off'};
                 }
                 else{
-                    selection.splice(selection.indexOf(d),1);
-                    d3.select(this).style("opacity", 1);
+                    selection.splice(selection.indexOf(column[i]+dataset[j][column[i]]),1);
+                    // d3.select(this).style("opacity", 1);
                     data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'on'};
                 }
-                $.post("", data_toserver, function(){})
-                xhr.open('GET', '', true);
-                xhr.send('');
+                $.post("", data_toserver, function(data_infunc){
+                    data_received = data_infunc;
+                    redraw();
+                });
             });
 
         let text = svg_elem.selectAll("text")
@@ -168,107 +165,3 @@ function redraw(){
 redraw();
 
 window.addEventListener('resize',redraw);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let dataset = data_fs;
-// let dataset_dev = data_device;
-
-// // Finding the max value in the Max column
-// let max_thp = dataset.map(function(o){return o.Max});
-
-// // let dataset = [8000, 10000, 5006, 12000, 18000, 3000, 4000, 12000, 16000];
-
-// let selection = [];
-
-
-
-// // 2nd part
-
-// // max_thp = dataset_dev.map(function(o){return o.Max});
-
-// // // let dataset_dev = [8000, 10000, 5006, 12000, 18000, 3000, 4000, 12000, 16000];
-
-// //  svgWidth = 500, svgHeight = 300, barPadding = 10;
-// //  barWidth = (svgWidth / dataset_dev.length);
-
-// //  linearScale = d3.scaleLinear()
-// //     .domain([0, d3.max(max_thp)])
-// //     .range([0,svgHeight-25]);
-
-// //  svg = d3.select('svg.var2')
-// //     .attr("width", svgWidth)
-// //     .attr("height", svgHeight);
-    
-// //  barChart = svg.selectAll("rect")
-// //     .data(dataset_dev)
-// //     .enter()
-// //     .append("rect")
-// //     .attr("y", function(d) {
-// //          return svgHeight - linearScale(d.Max - d.Min) 
-// //     })
-// //     .attr("height", function(d) { 
-// //         return linearScale(d.Max - d.Min); 
-// //     })
-// //     .attr("width", barWidth - barPadding)
-// //     .attr("transform", function (d, i) {
-// //         let translate = [barWidth * i, 0]; 
-// //         return "translate("+ translate +")";
-// //     })
-// //     .on("click", function(d){
-// //         if(selection.indexOf(d) == -1){
-// //             selection.push(d);
-// //             d3.select(this).style("opacity", 0.5);
-// //         }
-// //         else{
-// //             selection.splice(selection.indexOf(d),1);
-// //             d3.select(this).style("opacity", 1);
-// //         }
-// //     });
-
-//  text = svg.selectAll("text")
-//     .data(dataset_dev)
-//     .enter()
-//     .append("text")
-//     .text(function(d) {
-//         return Math.round(d.Max - d.Min);
-//     })
-//     .attr("y", function(d, i) {
-//         return svgHeight - linearScale(d.Max - d.Min) - 2;
-//     })
-//     .attr("x", function(d, i) {
-//         return barWidth * i;
-//     })
-//     .attr("fill", "#A64C38");
-
-let pressed1 = false;
-
-let form1 = d3.select("#options1")
-    .append("form")    
-
-form1.append("input")
-    .attr("type", "button")
-    .attr("name", "toggle")
-    .attr("value", "FileSystem")
-    .on("click", function(){
-        console.log("pressed");
-        if(!pressed1){
-            barChart.style("opacity", 0.5)
-            pressed1 = true;
-        }
-        else{
-            barChart.style("opacity", 1)
-            pressed1 = false;
-        }
-    }); 
-
