2f63b705bc2e3a9b0299e6794c32b88289756056
tyagi-iiitv <anjul.ten@gmail.com>
2018-08-09 16:19:48 -0400

    Addded comments for app.py

--- app.py
+++ app.py
@@ -5,27 +5,36 @@ import pandas as pd
 
 app = Flask(__name__)
 
+'''
+This method is used to calculate the new group by details from the dataset for each new request.
+'''
 def calculations(filtered_data):
-    min_thp = 10000000000000000
-    max_thp = 0
-    max_col = 0
-    for col in columns[:-1]:
-        col_thp = [col]
-        col_thp.append('Throughput')
-        df_grouped = pd.DataFrame(filtered_data[col_thp].groupby(col).describe()).reset_index()
-        df_grouped['Max'] = df_grouped[('Throughput', 'max')]
-        df_grouped['Min'] = df_grouped[('Throughput', 'min')]
-        new_chart_df = df_grouped[[col, 'Max', 'Min']]
-        new_chart_df.columns = new_chart_df.columns.droplevel(1)
-        chart_df = dataframes[col]
-        chart_df.loc[chart_df[col].isin(new_chart_df[col]), ['Max','Min']] = new_chart_df[['Max','Min']]
-        max_thp = max(max_thp, new_chart_df['Max'].max())
-        min_thp = min(min_thp, new_chart_df['Min'].min())
-        max_col = max_col + chart_df.shape[0]
-        chart_df = chart_df.to_dict(orient='records')
-        chart_df = json.dumps(chart_df)
-        data_tosend[col] = chart_df
-    data_tosend['Max Cols'] = max_col
+    min_thp = 10000000000000000 # Min thp to a large value to find the min thp
+    max_thp = 0 # Max thp to a large value to find the Max thp
+    max_col = 0 # Maximum number of categories in the dataset. These are number of bars which will be displayed on the plot
+    for col in columns[:-1]: # Columns include Throughput as well, so -1 to exclude Throughput
+        col_thp = [col] # Create a list of columns to group by. The columns are grouped by the feature type.
+        col_thp.append('Throughput') # Add Throughput to the list. For example col is "workload", then col_thp will contain [Workload, Throughput]
+        df_grouped = pd.DataFrame(filtered_data[col_thp].groupby(col).describe()).reset_index() # Calculate the statistics for Throughput for the given column
+        df_grouped['Max'] = df_grouped[('Throughput', 'max')] # Get the Max Throughput for current column
+        df_grouped['Min'] = df_grouped[('Throughput', 'min')] # Get the Min throughput
+        new_chart_df = df_grouped[[col, 'Max', 'Min']] # Filter the columns that we need and remove extra information
+        new_chart_df.columns = new_chart_df.columns.droplevel(1) # Because it is a grouped by object, it contains multiple level of columns. So here we filter extra levels that we don't need
+        max_thp = max(max_thp, new_chart_df['Max'].max()) # Update the Max Throughput value which is the max thp in all of the current dataset
+        min_thp = min(min_thp, new_chart_df['Min'].min()) # Similarly update the min throughput value
+        chart_df = dataframes[col] # Dataframe is a dictionary of grouped by dataframes from the original dataset. 
+        '''
+            Because we want to keep the bar that the user clicks, we don't remove the clicked bar from the dataset but instead, we don't include the "off" bars in the Throughput calculations. Since chart_df in this case consistes of values from the original dataset, hence it contains all the values. But on the other hand, new_chart_df contains only the "on" bars and updated throughput values. Hence we need to update chart_df with the values from new_chart_df but keep every other row in chart_df intact. This will allow us to change the "on" bars based on user selection but no bars will dissappear since we keep the original dataset intact.
+        '''
+        chart_df.loc[chart_df[col].isin(new_chart_df[col]), ['Max','Min']] = new_chart_df.loc[new_chart_df[col].isin(chart_df[col]),['Max','Min']].values
+        '''
+            In the above line, we update chart_df with new updated throughput values from new_chart_df. All the rows that are in new_chart_df are updated in chart_df. On the other hand, the rows that are not in new_chart_df, which are the bars that are turned "off" by the user, are kept intact as they were in the original dataset i.e. in chart_df.
+        '''
+        max_col = max_col + chart_df.shape[0] # Add the number of categories in this Variable to Max Cols i.e. total number of bars to be displayed
+        chart_df = chart_df.to_dict(orient='records') # Create a dictionary of the dataframe to send to the frontend to draw bars
+        chart_df = json.dumps(chart_df) # Convert the dictionary to JSON object
+        data_tosend[col] = chart_df # Add the information in a Json array for the given variable
+    data_tosend['Max Cols'] = max_col # Add more information about the Overall dataset which is useful for plotting the bar on the right side.
     data_tosend['Max Thp'] = max_thp
     data_tosend['Min Thp'] = min_thp
 
@@ -38,49 +47,48 @@ def calculations(filtered_data):
 #     print(column_received)
 #     return 'OK'
     
-@app.route("/", methods = ['POST', 'GET'])
+@app.route("/", methods = ['POST', 'GET']) # View function for the app
 def index():
-    if request.method == 'POST':
-        column_received = request.form['column']
-        value_received = request.form['value']
-        switch_received = request.form['switch']
+    if request.method == 'POST': # Collect the Post request when the user clicks a bar
+        column_received = request.form['column'] # Get the variable selected by the user
+        value_received = request.form['value'] # Get the category within the variable selected by the user
+        switch_received = request.form['switch'] # "on" or "off"
         print(column_received)
         print(value_received)
         print(switch_received)
-        filtered_data = data
-        # Modifying the values based on selected bars
-        
-        if switch_received == 'off':
+        filtered_data = data # Create a new dataframe what will only include "on" values in the dataset
+        # Unique values is a dictionary of unique values in a variable. We add the value 
+        if switch_received == 'off': # Remove the selected bar from unique_values dict
             unique_values[column_received].remove(value_received)
-        else:
+        else: # Add the bar back to unique values dict
             unique_values[column_received].append(value_received)
         
-        for item in columns[:-1]:
+        for item in columns[:-1]: # for all columns except throughput, remove the values that are not in unique_values dict
             filtered_data = filtered_data[filtered_data[item].isin(unique_values[item])]
-        calculations(filtered_data)
+        calculations(filtered_data) 
         return jsonify(data_tosend)
-    else:
-        for item in columns[:-1]:
-            unique_items = list(data[item].unique())
+    else: # This method is executed each time the page is refreshed
+        for item in columns[:-1]: # For all columns except throughput
+            unique_items = list(data[item].unique()) # Add unique categories within each variable to unique_values dict
             unique_values[item] = unique_items
-        min_thp = 10000000000000000
-        max_thp = 0
-        max_cols = 0
-        for col in columns[:-1]:
-            col_thp = [col]
+        min_thp = 10000000000000000 # Assigning a large value to Min Thp initially
+        max_thp = 0 # Assigning a low value to Max Thp initially
+        max_cols = 0 # Max Cols hold the number of unique categories for all variables i.e. total number of bars to be displayed on the plot
+        for col in columns[:-1]: # For all columns except throughput
+            col_thp = [col] # Create a list with Throughput appended to the column name
             col_thp.append('Throughput')
-            df_grouped = pd.DataFrame(data[col_thp].groupby(col).describe()).reset_index()
-            df_grouped['Max'] = df_grouped[('Throughput', 'max')]
-            df_grouped['Min'] = df_grouped[('Throughput', 'min')]
-            chart_df = df_grouped[[col, 'Max', 'Min']]
-            dataframes[col] = chart_df
-            max_thp = max(max_thp, chart_df['Max'].max())
-            min_thp = min(min_thp, chart_df['Min'].min())
-            chart_df.columns = chart_df.columns.droplevel(1)
-            max_cols = max_cols+chart_df.shape[0]
-            chart_df = chart_df.to_dict(orient='records')
-            chart_df = json.dumps(chart_df)
-            data_tosend[col] = chart_df
+            df_grouped = pd.DataFrame(data[col_thp].groupby(col).describe()).reset_index() # Get the statistics for Throughput for the current variable (col)
+            df_grouped['Max'] = df_grouped[('Throughput', 'max')] # Get the max throughput from the calculated stats
+            df_grouped['Min'] = df_grouped[('Throughput', 'min')] # Get the min throughput
+            chart_df = df_grouped[[col, 'Max', 'Min']] # Filter the columns, we need only Max and Min at this moment
+            dataframes[col] = chart_df # Create a dict of dataframes and add the current dataframe to this dict for future use. This prevents us from doing the same calculations again and again.
+            max_thp = max(max_thp, chart_df['Max'].max()) # Update the Max Thp
+            min_thp = min(min_thp, chart_df['Min'].min()) # Update the Min Thp
+            chart_df.columns = chart_df.columns.droplevel(1) # Because the group by object consists of multiple levels of columns, we need to drop a level down for simplification
+            max_cols = max_cols+chart_df.shape[0] # Add number of categories under current variable to max_cols
+            chart_df = chart_df.to_dict(orient='records') # Conver the dataframe to dict
+            chart_df = json.dumps(chart_df) # Create a JSON object
+            data_tosend[col] = chart_df # Add the JSON object to JSON array 
         data_tosend['Max Cols'] = max_cols
         data_tosend['Max Thp'] = max_thp
         data_tosend['Min Thp'] = min_thp
@@ -88,12 +96,16 @@ def index():
 
 
 if __name__ == "__main__":
-    data = pd.read_csv('dataset/systems_data.csv')
-    columns = list(data.columns)
-    data_tosend = {'columns': columns[:-1]}
-    # Store the dataframes in a dictionary
-    dataframes = {}
-    # Calculating unique values in each column
+    '''
+        This method is run only once when the server is started
+    '''
+    data = pd.read_csv('dataset/systems_data.csv') # Reading the dataset is done only once when the server is started
+    columns = list(data.columns) # Get the columns in the dataframe and create a list of the column names
+    for col in columns[:-1]: # For all columns except throughput
+        data[col] = data[col].apply(str) # Change the datatype for each column to be of type string so that there are no conflicts when performing calculations on each of the columns
+    data_tosend = {'columns': columns[:-1]} # Data to send is the JSON dict which is sent to the client to draw the bars. Client need to know the column names for labelling of the bars
+    dataframes = {} # This variable stores all variables in the dataset and the statistics of throughput for each of these variables. It is a dict of dataframes
+    # Calculating unique values in each column and adding it to unique_values dict
     unique_values = {}
     for item in columns[:-1]:
         unique_items = list(data[item].unique())
--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,13 @@
 let column = data_imported.columns;
-
 let margin = {top: 120, right: 20, bottom: 0, left: 70};
-let result_svg_height = 500;
-let result_svg_width = 80;
-let result_bar_width = 70;
-let svgHeight = 800 - margin.left - margin.right, barPadding = 10;
+let margin_result = {top: 120, right: 20, bottom: 0, left: 20};
+let result_svg_height = 730;
+let result_svg_width = 100;
+let result_bar_width = 60;
+let svgHeight = 730, barPadding = 10;
 let max_cols = data_imported['Max Cols'];
 let barWidth = (parseFloat(document.getElementById('area1').offsetWidth)/(max_cols)-4);
-let data_received;
+let data_received = data_imported;
 let colorScale = d3.scaleOrdinal(d3.schemeCategory10)
     .domain(column);
 let selection = [];
@@ -24,7 +24,7 @@ function redraw(){
 
     $('#area1').empty();   
     $('#area2').empty();
-
+    // console.log(selection)
     let global_bar_translate = 0;
     let global_text_translate = 0;
 
@@ -33,13 +33,14 @@ function redraw(){
         .attr("width", '100%')
         .attr("height", svgHeight)
         .attr("border",1)
+        .attr("class", "main-svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-
+    column = data_received.columns
     for(let i in column){
         // Finding the max throughput for the given variable
-        let dataset = JSON.parse(data_imported[column[i]]);
+        let dataset = JSON.parse(data_received[column[i]]);
         let max_thp = dataset.map(function(o){return o.Max});
         // Main code to create plots using the data
         let g = svg_elem.append('g');
@@ -77,16 +78,20 @@ function redraw(){
             .on("click", function(d,j){
                 let data_toserver;
                 let is_present = false;
+                let index_obtained = 0;
                 for(let k in selection){
-                    if(selection[k].id == column[i]+dataset[j][column[i]])
-                    is_present = true;
+                    if(selection[k].id == column[i]+dataset[j][column[i]]){
+                        is_present = true;
+                        index_obtained = k;
+                        break;
+                    }
                 }
                 if(!is_present){
                     selection.push({'id': column[i]+dataset[j][column[i]]});
                     data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'off'};
                 }
                 else{
-                    selection.splice(selection.indexOf(column[i]+dataset[j][column[i]]),1);
+                    selection.splice(index_obtained,1);
                     data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'on'};
                 }
                 $.post("", data_toserver, function(data_infunc){
@@ -104,10 +109,9 @@ function redraw(){
                 return dataset[j][column[i]];
             })
             .attr("transform", function (d, j) {
-                console.log(global_text_translate)
-                let translate = [barWidth * global_text_translate+17, 460]
+                let translate = [barWidth * global_text_translate+20, 460]
                 global_text_translate++
-                return "translate("+ translate +")rotate(90)";
+                return "translate("+ translate +")rotate(45)";
             })
             
             
@@ -120,6 +124,7 @@ function redraw(){
             .attr("type", "button")
             .attr("name", "toggle")
             .attr("value", column[i])
+            .attr("width", 40)
             .on("click", function(event){
                 let data_button_tosend;
                 if(!pressed1){
@@ -141,32 +146,28 @@ function redraw(){
 
     // ****************************** Below is 2nd column code ******************************
 
-    // let resultsLinearScale = d3.scaleLinear()
-    //     .domain([0, data_imported['Max Thp']])
-    //     .range([0, result_svg_height-25]);
-
-    // let svg_result = d3.select('#area2').append('div').append('svg')
-    //     .attr("width", result_svg_width)
-    //     .attr("height", result_svg_height);
+    let svg_result = d3.select('#area2').append('div').append('svg')
+        .attr("width", result_svg_width)
+        .attr("height", result_svg_height)
+        .attr("class", "result-svg")
+        .append("g")
+            .attr("transform", "translate(" + margin_result.left + "," + margin_result.top + ")");
     
-    // let resultBarChart = svg_result.selectAll("rect")
-    //     .data([data_imported['Max Thp'] - data_imported['Min Thp']])
-    //     .enter()
-    //     .append("rect")
-    //     .attr("y", function(d) {
-    //         return result_svg_height - resultsLinearScale(d); 
-    //     })
-    //     .attr("height", function(d) { 
-    //         return resultsLinearScale(d); 
-    //     })
-    //     .attr("width", result_bar_width - barPadding)
-    //     .attr("transform", function (d, i) {
-    //         let translate = [result_bar_width * i, 0]; 
-    //         return "translate("+ translate +")";
-    //     });
+    svg_result.append('g').append("rect")
+        .attr("y", function(d) {
+            return linearScale(data_received['Max Thp']); 
+        })
+        .attr("height", function(d) { 
+            return linearScale(data_received['Min Thp']) - linearScale(data_received['Max Thp']); 
+        })
+        .attr("width", result_bar_width - barPadding)
+        .attr("transform", function (d, i) {
+            let translate = [result_bar_width * i, 0]; 
+            return "translate("+ translate +")";
+        });
         
 
-    // let resultText = svg_result.selectAll("text")
+    // let resultText = svg_result.append('g').selectAll("text")
     //     .data([data_imported['Max Thp'] - data_imported['Min Thp']])
     //     .enter()
     //     .append("text")
