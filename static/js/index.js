da2d0e8d5da843a3c7cde943e2acc57986b3baf3
tyagi-iiitv <anjul.ten@gmail.com>
2018-08-09 16:50:15 -0400

    Added comments to javascript file

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,27 @@
-let column = data_imported.columns;
-let margin = {top: 120, right: 20, bottom: 0, left: 70};
-let margin_result = {top: 120, right: 20, bottom: 0, left: 20};
-let result_svg_height = 730;
-let result_svg_width = 100;
-let result_bar_width = 60;
-let svgHeight = 730, barPadding = 10;
-let max_cols = data_imported['Max Cols'];
-let barWidth = (parseFloat(document.getElementById('area1').offsetWidth)/(max_cols)-4);
-let data_received = data_imported;
-let colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+let column = data_imported.columns; // Get the column names from the dataset except throughput
+let margin = {top: 120, right: 20, bottom: 0, left: 70}; // Set the margins for the svg object
+let margin_result = {top: 120, right: 20, bottom: 0, left: 20}; // Set the margins for the result svg object, which is the svg in "area2" of the index.html file
+let result_svg_height = 730; // Height of the svg in Area2
+let result_svg_width = 100; // Width of svg in Area2
+let result_bar_width = 60; // Bar width of svg in Area2
+let svgHeight = 730, barPadding = 10; // Svg height and bar padding for Area1
+let max_cols = data_imported['Max Cols']; // Get the maximum number of bars that are to be drawn in Area1
+let barWidth = (parseFloat(document.getElementById('area1').offsetWidth)/(max_cols)-4); // Calculate bar width by dividing the width of Area1 by max cols
+let data_received = data_imported; // Data received captures the new data when user clicks on some bar or a button. Initially it is equal to original dataset but later it changes.
+let colorScale = d3.scaleOrdinal(d3.schemeCategory10) // To add different colors to bars representing each variable in the dataset
     .domain(column);
-let selection = [];
-let linearScale = d3.scaleLinear()
+let selection = []; // This array helps keep track of which bar the user clicked
+let linearScale = d3.scaleLinear() // Scale to scale throughput values to the height of svg
     .domain([data_imported['Min Thp'], data_imported['Max Thp']])
     .range([svgHeight-300,0])
 
-let xScale = d3.scaleBand()
-    .range([0, max_cols])
+function redraw(){ // Redraws every bar when the user makes a selection
 
-let xAxis = d3.svg
- 
-function redraw(){
-
-    $('#area1').empty();   
-    $('#area2').empty();
-    // console.log(selection)
-    let global_bar_translate = 0;
-    let global_text_translate = 0;
+    $('#area1').empty(); // Delete every object in Area 1 to redraw everything  
+    $('#area2').empty(); // Delete every object in Area 2
+    
+    let global_bar_translate = 0; // To draw each bar next to each other
+    let global_text_translate = 0; // To write the labels for each bar underneath
 
     // Declaring the svg element in area 1
     let svg_elem = d3.select('#area1').append('svg')
@@ -37,15 +32,15 @@ function redraw(){
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    column = data_received.columns
+    column = data_received.columns  // Get the column names from the dataset
     for(let i in column){
         // Finding the max throughput for the given variable
-        let dataset = JSON.parse(data_received[column[i]]);
-        let max_thp = dataset.map(function(o){return o.Max});
-        // Main code to create plots using the data
-        let g = svg_elem.append('g');
+        let dataset = JSON.parse(data_received[column[i]]); // Each element in data_received is the throughput details about a variable in the dataset. Extracting this information in a for loop
+        let max_thp = dataset.map(function(o){return o.Max}); // Get the max value of throughput for current column
 
-        let barChart = g.selectAll("rect")
+        let g = svg_elem.append('g'); // Append a group element to the svg.
+
+        let barChart = g.selectAll("rect") // Draw the bars to the current g element inside the svg in Area1
             .data(dataset)
             .enter()
             .append("rect")
@@ -59,8 +54,8 @@ function redraw(){
             .attr("width", barWidth - barPadding)
             .attr("opacity", function(d, j){
                 let is_present = false;
-                for(let k in selection){
-                    if(selection[k].id == column[i]+dataset[j][column[i]])
+                for(let k in selection){  // Check if the element is present in selection array, is yes, then plot if with opacity 0.5, otherwise plot it with opacity 1
+                    if(selection[k].id == column[i]+dataset[j][column[i]]) 
                     is_present = true;
                 }
                 if(!is_present){
@@ -79,29 +74,29 @@ function redraw(){
                 let data_toserver;
                 let is_present = false;
                 let index_obtained = 0;
-                for(let k in selection){
+                for(let k in selection){ // Check if the current selection by the user is present in selection array
                     if(selection[k].id == column[i]+dataset[j][column[i]]){
                         is_present = true;
                         index_obtained = k;
                         break;
                     }
                 }
-                if(!is_present){
+                if(!is_present){ // Add the element to selection array if it is not present and send the information to the server
                     selection.push({'id': column[i]+dataset[j][column[i]]});
                     data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'off'};
                 }
-                else{
+                else{ // Remove the element from selection array and send information to the server
                     selection.splice(index_obtained,1);
                     data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'on'};
                 }
                 $.post("", data_toserver, function(data_infunc){
-                    data_received = data_infunc;
-                    redraw();
+                    data_received = data_infunc; // The server returns new throughput values based on current user selection, update data_received with received information
+                    redraw(); // Redraw the bars based on current received information
                 });
             });
         
-        let g_text = svg_elem.append('g')    
-        let text = g_text.selectAll("text")
+        let g_text = svg_elem.append('g')     
+        let text = g_text.selectAll("text") // Add the text below each bar
             .data(dataset)
             .enter()
             .append("text")
@@ -114,6 +109,7 @@ function redraw(){
                 return "translate("+ translate +")rotate(45)";
             })
             
+        // ****************************** Below is the code for buttons ******************************
             
         let pressed1 = false;
 
@@ -146,14 +142,14 @@ function redraw(){
 
     // ****************************** Below is 2nd column code ******************************
 
-    let svg_result = d3.select('#area2').append('div').append('svg')
+    let svg_result = d3.select('#area2').append('div').append('svg') // Initialize the svg element in Area2
         .attr("width", result_svg_width)
         .attr("height", result_svg_height)
         .attr("class", "result-svg")
         .append("g")
             .attr("transform", "translate(" + margin_result.left + "," + margin_result.top + ")");
     
-    svg_result.append('g').append("rect")
+    svg_result.append('g').append("rect") // Add the result bar
         .attr("y", function(d) {
             return linearScale(data_received['Max Thp']); 
         })
@@ -167,23 +163,9 @@ function redraw(){
         });
         
 
-    // let resultText = svg_result.append('g').selectAll("text")
-    //     .data([data_imported['Max Thp'] - data_imported['Min Thp']])
-    //     .enter()
-    //     .append("text")
-    //     .text(function(d) {
-    //         return d;
-    //     })
-    //     .attr("y", function(d, i) {
-    //         return result_svg_height - resultsLinearScale(d) - 2;
-    //     })
-    //     .attr("x", function(d, i) {
-    //         return result_bar_width * i;
-    //     })
-    //     .attr("fill", "#A64C38");
 
 }
 
-redraw();
+redraw(); // Call the redraw function everytime this script runs
 
-window.addEventListener('resize',redraw);
+window.addEventListener('resize',redraw); // Bootstrap will run everytime the user tries to resize the windw
