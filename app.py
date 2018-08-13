f7fce9bb4870f1a1954f0cb58de26fa4a52b6c5f
tyagi-iiitv <anjul.ten@gmail.com>
2018-08-13 13:29:27 -0400

    Old Files

---
+++ app.py
@@ -0,0 +1,107 @@
+import json
+
+from flask import Flask, render_template, request, redirect, Response, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+'''
+This method is used to calculate the new group by details from the dataset for each new request.
+'''
+def calculations(filtered_data):
+    min_thp = 10000000000000000
+    max_thp = 0
+    max_col = 0
+    for col in columns[:-1]:
+        col_thp = [col]
+        col_thp.append('Throughput')
+        df_grouped = pd.DataFrame(filtered_data[col_thp].groupby(col).describe()).reset_index()
+        df_grouped['Max'] = df_grouped[('Throughput', 'max')]
+        df_grouped['Min'] = df_grouped[('Throughput', 'min')]
+        new_chart_df = df_grouped[[col, 'Max', 'Min']]
+        new_chart_df.columns = new_chart_df.columns.droplevel(1)
+        max_thp = max(max_thp, new_chart_df['Max'].max())
+        min_thp = min(min_thp, new_chart_df['Min'].min())
+        chart_df = dataframes[col]
+        chart_df.loc[chart_df[col].isin(new_chart_df[col]), ['Max','Min']] = new_chart_df.loc[new_chart_df[col].isin(chart_df[col]),['Max','Min']].values
+        max_col = max_col + chart_df.shape[0]
+        chart_df = chart_df.to_dict(orient='records')
+        chart_df = json.dumps(chart_df)
+        data_tosend[col] = chart_df
+    print('Min Thp ' + str(min_thp))
+    print('Max Thp ' + str(max_thp))
+    data_tosend['Max Cols'] = max_col
+    data_tosend['Max Thp'] = max_thp
+    data_tosend['Min Thp'] = min_thp
+
+    
+# @app.route('/button', methods = ['POST', 'GET'])
+# def button_worker():
+#     column_received = request.form['column']
+#     switch_received = request.form['switch']
+#     print(switch_received)
+#     print(column_received)
+#     return 'OK'
+    
+@app.route("/", methods = ['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        column_received = request.form['column']
+        value_received = request.form['value']
+        switch_received = request.form['switch']
+        print(column_received)
+        print(value_received)
+        print(switch_received)
+        filtered_data = data
+        # Modifying the values based on selected bars
+        if switch_received == 'off':
+            unique_values[column_received].remove(value_received)
+        else:
+            unique_values[column_received].append(value_received)
+        
+        for item in columns[:-1]:
+            filtered_data = filtered_data[filtered_data[item].isin(unique_values[item])]
+        calculations(filtered_data)
+        return jsonify(data_tosend)
+    else:
+        for item in columns[:-1]:
+            unique_items = list(data[item].unique())
+            unique_values[item] = unique_items
+        min_thp = 10000000000000000
+        max_thp = 0
+        max_cols = 0
+        for col in columns[:-1]:
+            col_thp = [col]
+            col_thp.append('Throughput')
+            df_grouped = pd.DataFrame(data[col_thp].groupby(col).describe()).reset_index()
+            df_grouped['Max'] = df_grouped[('Throughput', 'max')]
+            df_grouped['Min'] = df_grouped[('Throughput', 'min')]
+            chart_df = df_grouped[[col, 'Max', 'Min']]
+            dataframes[col] = chart_df
+            max_thp = max(max_thp, chart_df['Max'].max())
+            min_thp = min(min_thp, chart_df['Min'].min())
+            chart_df.columns = chart_df.columns.droplevel(1)
+            max_cols = max_cols+chart_df.shape[0]
+            chart_df = chart_df.to_dict(orient='records')
+            chart_df = json.dumps(chart_df)
+            data_tosend[col] = chart_df
+        data_tosend['Max Cols'] = max_cols
+        data_tosend['Max Thp'] = max_thp
+        data_tosend['Min Thp'] = min_thp
+        return render_template("index.html", data=data_tosend)
+
+
+if __name__ == "__main__":
+    data = pd.read_csv('dataset/systems_data.csv')
+    columns = list(data.columns)
+    for col in columns[:-1]:
+        data[col] = data[col].apply(str)
+    data_tosend = {'columns': columns[:-1]}
+    # Store the dataframes in a dictionary
+    dataframes = {}
+    # Calculating unique values in each column
+    unique_values = {}
+    for item in columns[:-1]:
+        unique_items = list(data[item].unique())
+        unique_values[item] = unique_items
+    app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,189 @@
+let column = data_imported.columns;
+let margin = {top: 120, right: 20, bottom: 0, left: 70};
+let margin_result = {top: 120, right: 20, bottom: 0, left: 20};
+let result_svg_height = 730;
+let result_svg_width = 100;
+let result_bar_width = 60;
+let svgHeight = 730, barPadding = 10;
+let max_cols = data_imported['Max Cols'];
+let barWidth = (parseFloat(document.getElementById('area1').offsetWidth)/(max_cols)-4);
+let data_received = data_imported;
+let colorScale = d3.scaleOrdinal(d3.schemeCategory10)
+    .domain(column);
+let selection = [];
+let linearScale = d3.scaleLinear()
+    .domain([data_imported['Min Thp'], data_imported['Max Thp']])
+    .range([svgHeight-300,0])
+
+let xScale = d3.scaleBand()
+    .range([0, max_cols])
+
+let xAxis = d3.svg
+ 
+function redraw(){
+
+    $('#area1').empty();   
+    $('#area2').empty();
+    // console.log(selection)
+    let global_bar_translate = 0;
+    let global_text_translate = 0;
+
+    // Declaring the svg element in area 1
+    let svg_elem = d3.select('#area1').append('svg')
+        .attr("width", '100%')
+        .attr("height", svgHeight)
+        .attr("border",1)
+        .attr("class", "main-svg")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    column = data_received.columns
+    for(let i in column){
+        // Finding the max throughput for the given variable
+        let dataset = JSON.parse(data_received[column[i]]);
+        let max_thp = dataset.map(function(o){return o.Max});
+        // Main code to create plots using the data
+        let g = svg_elem.append('g');
+
+        let barChart = g.selectAll("rect")
+            .data(dataset)
+            .enter()
+            .append("rect")
+            .attr("y", function(d) {
+                return linearScale(d.Max); 
+            })
+            .attr("height", function(d) { 
+                return linearScale(d.Min) - linearScale(d.Max); 
+            })
+            .attr("fill", colorScale(i))
+            .attr("width", barWidth - barPadding)
+            .attr("opacity", function(d, j){
+                let is_present = false;
+                for(let k in selection){
+                    if(selection[k].id == column[i]+dataset[j][column[i]])
+                    is_present = true;
+                }
+                if(!is_present){
+                    return 1
+                }
+                else{
+                    return 0.5
+                }
+            })
+            .attr("transform", function (d, j) {
+                let translate = [barWidth * global_bar_translate+10, 0];
+                global_bar_translate++; 
+                return "translate("+ translate +")";
+            })
+            .on("click", function(d,j){
+                let data_toserver;
+                let is_present = false;
+                let index_obtained = 0;
+                for(let k in selection){
+                    if(selection[k].id == column[i]+dataset[j][column[i]]){
+                        is_present = true;
+                        index_obtained = k;
+                        break;
+                    }
+                }
+                if(!is_present){
+                    selection.push({'id': column[i]+dataset[j][column[i]]});
+                    data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'off'};
+                }
+                else{
+                    selection.splice(index_obtained,1);
+                    data_toserver = {'column': column[i], 'value': dataset[j][column[i]], 'switch': 'on'};
+                }
+                $.post("", data_toserver, function(data_infunc){
+                    data_received = data_infunc;
+                    redraw();
+                });
+            });
+        
+        let g_text = svg_elem.append('g')    
+        let text = g_text.selectAll("text")
+            .data(dataset)
+            .enter()
+            .append("text")
+            .text(function(d,j) {
+                return dataset[j][column[i]];
+            })
+            .attr("transform", function (d, j) {
+                let translate = [barWidth * global_text_translate+20, 460]
+                global_text_translate++
+                return "translate("+ translate +")rotate(90)";
+            })
+            
+            
+        let pressed1 = false;
+
+        let form1 = d3.select('#area1').append("form")
+            .attr('class','btn-group')    
+        
+        form1.append("input")
+            .attr("type", "button")
+            .attr("name", "toggle")
+            .attr("value", column[i])
+            .attr("width", 40)
+            .on("click", function(event){
+                let data_button_tosend;
+                if(!pressed1){
+                    barChart.style("opacity", 0.5)
+                    pressed1 = true;
+                    data_button_tosend = {'column': column[i], 'switch': 'off'};
+                }
+                else{
+                    barChart.style("opacity", 1)
+                    pressed1 = false;
+                    data_button_tosend = {'column': column[i], 'switch': 'on'};
+                }
+                $.post("button", data_button_tosend, function(){});
+            });
+    }
+    svg_elem.append('g')
+        .call(d3.axisLeft(linearScale))
+
+
+    // ****************************** Below is 2nd column code ******************************
+
+    let svg_result = d3.select('#area2').append('div').append('svg')
+        .attr("width", result_svg_width)
+        .attr("height", result_svg_height)
+        .attr("class", "result-svg")
+        .append("g")
+            .attr("transform", "translate(" + margin_result.left + "," + margin_result.top + ")");
+    
+    svg_result.append('g').append("rect")
+        .attr("y", function(d) {
+            return linearScale(data_received['Max Thp']); 
+        })
+        .attr("height", function(d) { 
+            return linearScale(data_received['Min Thp']) - linearScale(data_received['Max Thp']); 
+        })
+        .attr("width", result_bar_width - barPadding)
+        .attr("transform", function (d, i) {
+            let translate = [result_bar_width * i, 0]; 
+            return "translate("+ translate +")";
+        });
+        
+
+    // let resultText = svg_result.append('g').selectAll("text")
+    //     .data([data_imported['Max Thp'] - data_imported['Min Thp']])
+    //     .enter()
+    //     .append("text")
+    //     .text(function(d) {
+    //         return d;
+    //     })
+    //     .attr("y", function(d, i) {
+    //         return result_svg_height - resultsLinearScale(d) - 2;
+    //     })
+    //     .attr("x", function(d, i) {
+    //         return result_bar_width * i;
+    //     })
+    //     .attr("fill", "#A64C38");
+
+}
+
+redraw();
+
+window.addEventListener('resize',redraw);
